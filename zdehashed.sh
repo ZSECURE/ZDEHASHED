d6585f7f41735ec3c5b72a35c8874377e045f602
Zak Clifford <46941177+ZSECURE@users.noreply.github.com>
2022-12-28 22:27:03 +0000

    Added usage info

--- zdehashed.sh
+++ zdehashed.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Usage function
+function usage {
+    echo "Usage: dehashed <search_domain> <save_location>"
+    echo "Example: dehashed example.com results.json"
+    exit 1
+}
+
+# Exit if not present
+if [ -z "$SearchDomain" ] || [ -z "$SaveLocation" ]
+then
+    usage
+fi
+
 # Setting Variables
 APIKey="email_address:api_key"
 SearchDomain=$1
