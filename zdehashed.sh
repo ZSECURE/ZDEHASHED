cb9079907bbbdb95589aa72c8c072bf008d1607c
Zak Clifford <46941177+ZSECURE@users.noreply.github.com>
2024-07-07 13:39:21 +0100

    Update zdehashed.sh

--- zdehashed.sh
+++ zdehashed.sh
@@ -2,26 +2,39 @@
 
 # Usage function
 function usage {
-    echo "Usage: dehashed <search_domain> <save_location>"
-    echo "Example: dehashed example.com results.json"
+    echo "Usage: dehashed <search_domain>"
+    echo "Example: dehashed example.com"
     exit 1
 }
 
+# Setting Variables
+APIKey="API_Key_Goes_Here"
+SearchDomain=$1
+
 # Exit if not present
-if [ -z "$SearchDomain" ] || [ -z "$SaveLocation" ]
+if [ -z "$SearchDomain" ]
 then
     usage
 fi
 
-# Setting Variables
-APIKey="email_address:api_key"
-SearchDomain=$1
-SaveLocation=$2
+# Function to create directory and save location
+function create_save_location {
+    local domain_dir=$(echo $SearchDomain | tr '.' '-')
+    local timestamp=$(date +"%Y%m%d%H%M%S")
+    local save_dir="/opt/zdehashed/domains/$domain_dir"
+    local save_location="$save_dir/results-$timestamp.json"
+
+    mkdir -p $save_dir
+
+    echo $save_location
+}
+
 
 #Creating function
 function dehashed {
+        local save_location=$(create_save_location)
 
-        curl --silent "https://api.dehashed.com/search?query=domain:$SearchDomain" -u $APIKey  -H 'Accept: application/json' -o $SaveLocation
+        curl --silent "https://api.dehashed.com/search?query=domain:$SearchDomain" -u $APIKey  -H 'Accept: application/json' -o $save_location
 
         }
 
